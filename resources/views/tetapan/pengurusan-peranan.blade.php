ad909a7bee2c06979354e7202ecec5100a003cae
hazmi-rifhan <hazmi@rifhan.com>
2025-10-08 11:57:49 +0800

    add bootstrap-dialog plugin

--- resources/views/tetapan/pengurusan-peranan.blade.php
+++ resources/views/tetapan/pengurusan-peranan.blade.php
@@ -23,7 +23,7 @@
 				<div class="row mt-3">
 					@foreach ($roles as $roles)
 						<div class="col-lg-3 col-sm-4 fadein-top">
-							<div class="card popup-btn" style="background-color:#DCDCDC">
+							<div class="card popup-btn" style="background-color:#DCDCDC" onclick="testDialog(2);">
 								<div class="card-body">
 									<div class="row">
 										<div class="col-sm-4">
@@ -49,6 +49,37 @@
 
 @push('scripts')
 <script>
+
+	testDialog = function(process,data)
+	{
+		if(process == '2')
+		{
+			let param = $.param({
+				tax_id: data
+			});
+
+			var title = 'Kemaskini Akses';
+			var target = '#application.apps_setting.homepath#module/g-fix-asset/insurance/tax/form.cfm?'+param;
+		}
+
+		BootstrapDialog.show({
+			size: BootstrapDialog.SIZE_WIDE,
+			type: 	BootstrapDialog.TYPE_DEFAULT,
+			title: title,
+			cssClass: 'application-info',
+			// message: $('<div></div>').load(target),
+			message: 'test',
+			closable: false,
+			closeByBackdrop: false,
+			closeByKeyboard: false,
+			buttons: [{
+				label: 'Tutup',
+				action: function(dialogRef){
+					dialogRef.close();
+				}
+			}]
+		});
+	}
 	
 </script>
 @endpush
